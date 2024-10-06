krille-chan/fluffychat

feat: Switch to firebase messaging package

scripts/enable-google-services.sh
@@ -1,0 +1,6 @@
+#!/usr/bin/env bash
+sed -i 's/#<GOOGLE_SERVICES>//g' pubspec.yaml
+sed -i 's,//<GOOGLE_SERVICES>,,g' android/build.gradle
+sed -i 's,//<GOOGLE_SERVICES>,,g' android/app/build.gradle
+sed -i 's,//<GOOGLE_SERVICES>,,g' lib/main.dart
+sed -i 's,//<GOOGLE_SERVICES>,,g' lib/utils/background_push.dart

staawi/client

scripts/enable-google-services.sh
@@ -1,0 +1,6 @@
+#!/usr/bin/env bash
+sed -i 's/#<GOOGLE_SERVICES>//g' pubspec.yaml
+sed -i 's,//<GOOGLE_SERVICES>,,g' android/settings.gradle
+sed -i 's,//<GOOGLE_SERVICES>,,g' android/app/build.gradle
+sed -i 's,//<GOOGLE_SERVICES>,,g' lib/main.dart
+sed -i 's,//<GOOGLE_SERVICES>,,g' lib/utils/background_push.dart