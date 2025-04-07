bbc9fd29fe774de6ad9765c8d368bf2c654e3f00
Peter Bwire <bwire517@gmail.com>
2025-04-07 17:04:08 +0300

    ensure asset/js folder exists in repo

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -4,7 +4,7 @@ OLM_VERSION=$(cat pubspec.yaml | yq .dependencies.flutter_olm | tr -d '"^')
 DOWNLOAD_PATH="https://github.com/famedly/olm/releases/download/v$OLM_VERSION/olm.zip"
 echo $DOWNLOAD_PATH
 
-cd assets/js/
+mkdir -p assets/js && cd assets/js/
 
 curl -L $DOWNLOAD_PATH > olm.zip
 unzip olm.zip && rm olm.zip
