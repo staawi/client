2d81b29258dc82334f8608b1913e3ccef098a68f
Peter Bwire <bwire517@gmail.com>
2025-04-07 20:40:41 +0300

    fix issue with signing keys during upload process

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 cd android
+echo $UPLOAD_SIGNING_KEY | base64 --decode --ignore-garbage > key.jks
+echo "storePassword=${UPLOAD_SIGNING_KEY_PASS}" >> key.properties
+echo "keyPassword=${UPLOAD_SIGNING_KEY_PASS}" >> key.properties
+echo "keyAlias=key" >> key.properties
+echo "storeFile=../key.jks" >> key.properties
 echo $PLAYSTORE_DEPLOY_KEY >> keys.json
 ls | grep key
 bundle install
