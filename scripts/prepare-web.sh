b5bdcf0fc5b94e4b7992c83cfcd05b47705c8e9f
Peter Bwire <bwire517@gmail.com>
2025-04-07 16:02:18 +0300

    correctly prepare web olm data

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -ve
 rm -rf assets/js/package
 
-OLM_VERSION=$(cat pubspec.yaml | yq .dependencies.flutter_olm)
+OLM_VERSION=$(cat pubspec.yaml | yq .dependencies.flutter_olm | tr -d '"^')
 DOWNLOAD_PATH="https://github.com/famedly/olm/releases/download/v$OLM_VERSION/olm.zip"
+echo $DOWNLOAD_PATH
 
 cd assets/js/ && curl -L $DOWNLOAD_PATH > olm.zip && cd ../../
 cd assets/js/ && unzip olm.zip && cd ../../
