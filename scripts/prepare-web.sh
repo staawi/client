52169d0c205671ddc4609a709f29cc6257fb1b76
Peter Bwire <bwire517@gmail.com>
2025-04-07 16:59:40 +0300

    minor change to prepare web commands

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -ve
-rm -rf assets/js/package
 
 OLM_VERSION=$(cat pubspec.yaml | yq .dependencies.flutter_olm | tr -d '"^')
 DOWNLOAD_PATH="https://github.com/famedly/olm/releases/download/v$OLM_VERSION/olm.zip"
 echo $DOWNLOAD_PATH
 
-cd assets/js/ && curl -L $DOWNLOAD_PATH > olm.zip && cd ../../
-cd assets/js/ && unzip olm.zip && cd ../../
-cd assets/js/ && rm olm.zip && cd ../../
-cd assets/js/ && mv javascript package && cd ../../
+cd assets/js/
+
+curl -L $DOWNLOAD_PATH > olm.zip
+unzip olm.zip && rm olm.zip
+rm -rf ./package && mv javascript package
+cd ../../
